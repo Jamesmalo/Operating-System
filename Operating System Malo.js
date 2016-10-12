9c52c2ff7842462d738257476adacbac7139fc52
Jamesmalo <9jmalo@stpatrick.org>
2016-10-12 11:28:03 -0500

    started work

---
+++ Operating System Malo.js
@@ -0,0 +1,53 @@
+function App(n,m,s){
+
+    //Declare your instance variables here.
+    this.name = n;
+    this.memory = m;
+    this.state = s;
+    if(this.state = 1){
+      this.state = this.open
+    }
+    else if(this.state = 2){
+      this.state = this.sleep
+    }
+    else if(this.state = 3){
+      this.state = this.active
+    }
+    else if(this.state = 4){
+      this.state = this.close
+    }
+    else(this.state < 4){
+      this.state = this.null
+    }
+    //Declare the instance function open here.
+    this.open = function(){
+
+
+    };
+    //Declare the instance function sleep here.
+    this.sleep = function(){
+
+
+    }
+    //Declare the instance function active here.
+    this.active = function(){
+
+
+    }
+    //Declare the instance function close here.
+    this.close = function(){
+
+
+
+    }
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
